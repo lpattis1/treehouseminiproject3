a2a121df2e5931ee399f703bee494ab1b2962829
lpattis1 <lpattis1@asu.edu>
2021-06-10 14:48:48 -0500

    Cleaned up code to follow the DRY principals.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Variables
+
 const main = document.querySelector("main");
 let html = ``;
 let red;
@@ -6,13 +7,33 @@ let green;
 let blue;
 let color;
 
+// ------------------------
+
+// Function
+
+/**
+ * A function that returns a randomized RGB color value.
+ * @param  {number} color - a parameter that supplements a specified color value.
+ * @return  {number} returns the randomized color.
+ */
+
+function randomColor(color) {
+  return Math.floor(Math.random() * color);
+}
+
+// ------------------------
+
 // Loop
+
 for (let i = 1; i <= 10; i++) {
-  red = Math.floor(Math.random() * 256);
-  green = Math.floor(Math.random() * 256);
-  blue = Math.floor(Math.random() * 256);
+  red = randomColor(256);
+  green = randomColor(256);
+  blue = randomColor(256);
   color = `rgb( ${red}, ${green}, ${blue} )`;
   html += `<div style="background-color: ${color}">${i}</div>`;
 }
 
+// ------------------------
+
+// Display Results
 main.innerHTML = html;
